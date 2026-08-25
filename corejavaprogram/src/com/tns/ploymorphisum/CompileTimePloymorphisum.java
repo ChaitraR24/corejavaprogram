1f8e09934b54df60430ce929018cd727b794a882
tnsda <tnsda@TNS_DA03>
2026-08-25 15:40:17 +0530

    commit

---
+++ corejavaprogram/src/com/tns/ploymorphisum/CompileTimePloymorphisum.java
@@ -0,0 +1,28 @@
+package com.tns.ploymorphisum;
+
+class Animal{
+	void sound() {
+		System.out.println("Animal makes sound");
+	}
+}
+
+class Cat extends Animal{
+	void sound() {
+		System.out.println("meows");
+	}
+}
+
+class Dog extends Animal{
+	void sound() {
+		System.out.println("dog barks");
+	}
+}
+
+public class CompileTimePloymorphisum {
+public static void main(String[] args) {
+	Animal animal = new Cat();
+	animal.sound();
+	animal = new Dog();
+	animal.sound();
+}
+}
